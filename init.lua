30406272dfb3ce53cf30187cbcde749336d23b27
Blockhead <jbis1337@hotmail.com>
2022-07-18 21:18:35 +1000

    Remove extraneous dofiles

--- init.lua
+++ init.lua
@@ -17,71 +17,9 @@ end
 
 
 -- Animals
-dofile(path .. "seal.lua") -- 
-dofile(path .. "hare.lua") -- 
-dofile(path .. "moose.lua") -- 
-dofile(path .. "crocodile.lua") -- 
-dofile(path .. "manatee.lua") -- 
-dofile(path .. "tiger.lua") -- 
-dofile(path .. "camel.lua") -- 
-dofile(path .. "elephant.lua") -- 
-dofile(path .. "carp.lua") -- 
-dofile(path .. "trout.lua") -- 
-dofile(path .. "blackbird.lua") -- 
-dofile(path .. "bear.lua") -- 
-dofile(path .. "boar.lua") -- 
 dofile(path .. "kangaroo.lua") -- 
-dofile(path .. "tortoise.lua") -- 
-dofile(path .. "hippo.lua") -- 
-dofile(path .. "shark.lua") -- 
-dofile(path .. "nandu.lua") -- 
-dofile(path .. "yak.lua") --
-dofile(path .. "spider.lua") --
-dofile(path .. "spidermale.lua") --
-dofile(path .. "crab.lua") --
-dofile(path .. "reindeer.lua") --
-dofile(path .. "volverine.lua") --
-dofile(path .. "owl.lua") --
-dofile(path .. "frog.lua") --
-dofile(path .. "monitor.lua") --
-dofile(path .. "gnu.lua") --
-dofile(path .. "puffin.lua") --
-dofile(path .. "anteater.lua") --
-dofile(path .. "hyena.lua") --
-dofile(path .. "rat.lua") --
-dofile(path .. "vulture.lua") --
-dofile(path .. "toucan.lua") --
-dofile(path .. "snowleopard.lua") --
-dofile(path .. "lobster.lua") --
-dofile(path .. "squid.lua") --
-dofile(path .. "kobra.lua") --
-dofile(path .. "bat.lua") --
-dofile(path .. "ant.lua") --
-dofile(path .. "termite.lua") --
-dofile(path .. "wasp.lua") --
-dofile(path .. "snail.lua") --
-dofile(path .. "locust.lua") --
-dofile(path .. "dragonfly.lua") --
-dofile(path .. "nymph.lua") --
-dofile(path .. "divingbeetle.lua") --
-dofile(path .. "olm.lua") --
-dofile(path .. "goldenmole.lua") --
-dofile(path .. "scorpion.lua") --
-dofile(path .. "goby.lua") --
-dofile(path .. "treelobster.lua") --
-dofile(path .. "notoptera.lua") --
-dofile(path .. "seahorse.lua") --
-dofile(path .. "trophies.lua") --
-dofile(path .. "tundravegetation.lua") --
-dofile(path .. "polarbear.lua") --
-dofile(path .. "muskox.lua") --
-dofile(path .. "fox.lua") --
-dofile(path .. "beluga.lua") --
-dofile(path .. "leopardseal.lua") --
-dofile(path .. "stellerseagle.lua") --
-dofile(path .. "otter.lua") --
-dofile(path .. "monkey.lua") --
-dofile(path .. "hunger.lua") --
+-- TODO: kangaroo steak
+--dofile(path .. "hunger.lua") --
 
 
 
